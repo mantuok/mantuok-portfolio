f7170ebe371ea1028832111c2a116cb6ea4c97dc
Svetlana Eliseeva <mantuok@gmail.com>
2024-11-09 20:48:38 -0800

    refactoring

--- app/components/Skills/skills.tsx
+++ app/components/Skills/skills.tsx
@@ -1,22 +1,5 @@
 import styles from "./skills.module.scss";
-import {
-  FaReact,
-  FaNode,
-  FaPython,
-  FaHtml5,
-  FaJs,
-  FaSass,
-} from "react-icons/fa";
-import {
-  SiNextdotjs,
-  SiTypescript,
-  SiFigma,
-  SiJest,
-  SiBootstrap,
-} from "react-icons/si";
-import { IconType } from "react-icons"; // Type for imported icon components
 
-// Define position type
 interface Position {
   top: number;
   left: number;
@@ -40,24 +23,9 @@ const Skills: React.FC = () => {
     "/img/icon_bootstrap5.svg",
   ];
 
-  // const icons: IconType[] = [
-  //   FaReact,
-  //   FaNode,
-  //   FaPython,
-  //   FaHtml5,
-  //   FaJs,
-  //   FaSass,
-  //   SiNextdotjs,
-  //   SiTypescript,
-  //   SiFigma,
-  //   SiJest,
-  //   SiBootstrap,
-  // ];
+  const radius = 20;
+  const containerSize = 500;
 
-  const radius = 20; // Control the spread of icons
-  const containerSize = 500; // Size of the container in pixels
-
-  // Store placed positions to check for overlaps
   const placedPositions: Position[] = [];
 
   const getRandomPosition = (index: number, totalIcons: number): Position => {
@@ -65,18 +33,15 @@ const Skills: React.FC = () => {
     let attempts = 0;
     const maxAttempts = 50;
 
-    // Loop to find a non-overlapping position
     do {
       const angle = index * ((Math.PI * 2) / totalIcons) + Math.random() * 0.5; // Slight randomness
       const distFromCenter = (radius - 45) + Math.random() * (containerSize / 2 - radius); // Vary distance
 
-      // Calculate position with random angle and distance
       const left = 50 + (distFromCenter / containerSize) * 100 * Math.cos(angle);
       const top = 50 + (distFromCenter / containerSize) * 100 * Math.sin(angle);
 
       position = { top, left };
 
-      // Check for overlap with existing icons
       attempts++;
     } while (
       attempts < maxAttempts &&
@@ -86,7 +51,7 @@ const Skills: React.FC = () => {
       )
     );
 
-    placedPositions.push(position); // Store position to check future overlaps
+    placedPositions.push(position); 
     return position;
   };
 
@@ -112,17 +77,3 @@ const Skills: React.FC = () => {
 };
 
 export default Skills;
-
-      // {/* {icons.map((IconComponent, index) => {
-      //   const position = getRandomPosition(index, icons.length);
-      //   return (
-      //     <IconComponent
-      //       key={index}
-      //       className={styles.icon}
-      //       style={{
-      //         top: `${position.top}%`,
-      //         left: `${position.left}%`,
-      //       }}
-      //     />
-      //   );
-      // })} */}
