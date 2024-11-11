fa6fefa6cd7acb0abf5f9a1ffb898c1f9f179873
Svetlana Eliseeva <mantuok@gmail.com>
2024-11-10 21:07:15 -0800

    projects data added

--- app/constants.tsx
+++ app/constants.tsx
@@ -23,3 +23,78 @@ export const NAV_ITEM = {
     href: "#contact",
   },
 };
+
+export const ProjectsData = [
+  {
+    id: 1,
+    name: "iUjex",
+    title: "Local Transport Company Web App",
+    description:
+      "This project enhances user experience for customers and drivers by connecting them through a multi-role web application.",
+    skillset:
+      "Using React.js, TypeScript, and Material UI, I implemented responsive interfaces and provide ongoing support to ensure the application functions correctly.",
+    image: "/img/project_iujex.png",
+    url: "",
+    github: "",
+  },
+  {
+    id: 2,
+    name: "Earthquake",
+    title: "Earthquake Preparedness Web App",
+    description:
+      "This project improves earthquake preparedness awareness in San Francisco with responsive user interfaces. ",
+    skillset:
+      "Using Next.js, TypeScript, and Chakra UI, I design an intuitive experience and ensure reliability through Jest testing.",
+    image: "/img/project_earthquake.png",
+    url: "",
+    github: "",
+  },
+  {
+    id: 3,
+    name: "Liga Bank",
+    title: "Local Bank Web App",
+    description:
+      "This project enhances the banking experience by providing tools for calculating mortgage and loan options.",
+    skillset:
+      "Using React, Redux, and Sass, I developed an  interface with efficient state management to ensure accurate calculations and a seamless user experience.",
+    image: "/img/project_ligabank.png",
+    url: "",
+    github: "",
+  },
+  {
+    id: 4,
+    name: "Avto Moto",
+    title: "Car Dealership Website",
+    description:
+      "This project improves the user experience for car buyers by providing a platform to explore vehicles and read customer reviews.",
+    skillset:
+      "Using React, Redux, and Sass, I developed an  interface that allows seamless browsing and effective state management for enhanced functionality.",
+    image: "/img/project_avtomoto.png",
+    url: "",
+    github: "",
+  },
+  {
+    id: 5,
+    name: "Cat Energy",
+    title: "Cat Food Manufacturer Website",
+    description:
+      "This project provides a responsive and adaptive web site for innovative cat food  and facilitates orders through catalog and order form.",
+    skillset:
+      "Using HTML5, LESS, and BEM methodology, I implemented clean and maintainable code to create an engaging and user-friendly interface.",
+    image: "/img/project_catenergy.png",
+    url: "",
+    github: "",
+  },
+  {
+    id: 6,
+    name: "Gllacy",
+    title: "Ice-Cream Parlour Website",
+    description:
+      "This project enhances user experience for customers and drivers by connecting them through a multi-role web application.",
+    skillset:
+      "Using HTML5 and CSS3, I built a visually appealing interface to engage users and highlight the brand’s offerings.",
+    image: "/img/project_gllacy.png",
+    url: "",
+    github: "",
+  },
+];
