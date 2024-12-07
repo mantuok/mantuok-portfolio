40c298f16a38c8bb25bfd269e8927019f182ac5f
Svetlana Eliseeva <mantuok@gmail.com>
2024-12-07 13:41:22 -0800

    added llama

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,12 @@ import About from "../app/components/About/about";
 import Projects from "../app/components/Projects/projects";
 import { ProjectsData } from "./constants";
 import Contact from "./components/Contact/contact";
+import Llama from "./components/Llama/llama";
 
 export default function Page() {
   return (
     <main>
+      <Llama />
       <Home />
       <About />
       {ProjectsData.map((project) => (
